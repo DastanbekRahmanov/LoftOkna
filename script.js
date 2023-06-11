78e1b08cdcd5011879dd4acc91d5051c8e93459e
Dastanbek <dastanbekrahmanov01@gmail.com>
2023-06-11 15:04:34 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+const goTopBtn = document.querySelector(".go-top");
+
+// обработчик на скролл окна
+window.addEventListener("scroll", trackScroll);
+// обработчик на нажатии
+goTopBtn.addEventListener("click", goTop);
+
+function trackScroll() {
+  // вычисляем положение от верхушки страницы
+  const scrolled = window.pageYOffset;
+  // считаем высоту окна браузера
+  const coords = document.documentElement.clientHeight;
+  // если вышли за пределы первого окна
+  if (scrolled > coords) {
+    // кнопка появляется
+    goTopBtn.classList.add("go-top--show");
+  } else {
+    // иначе исчезает
+    goTopBtn.classList.remove("go-top--show");
+  }
+}
+function goTop() {
+  // пока не вернулись в начало страницы
+  if (window.pageYOffset > 0) {
+    // скроллим наверх
+    window.scrollBy(0, -75); // второй аргумент - скорость
+    setTimeout(goTop, 0); // входим в рекурсию
+  }
+}
+
+function uslugHandler() {
+  window.scrollTo(0, 2150);
+}
+function contactHandler() {
+  window.scrollTo(0, 5100);
+}
+function nasHandler() {
+  window.scrollTo(0, 4750);
+}
